c3131f9877daf747e7b8973039420b5fb8761755
Jesus Llanos <jesussalirrosas.19@gmail.com>
2025-09-25 03:51:25 -0500

    Esta es la primera version de la seccion de asistencia

--- src/app/presentation/screens/control-asistencia/control-asistencia.ts
+++ src/app/presentation/screens/control-asistencia/control-asistencia.ts
@@ -1,11 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-control-asistencia',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './control-asistencia.html',
   styleUrl: './control-asistencia.css'
 })
 export class ControlAsistencia {
+  estadoAsistencia: string | null = null;
 
+  asistencias = [
+    { fecha: '2025-09-18', horaIngreso: '08:15', horaSalida: '17:10', estado: 'A tiempo' },
+    { fecha: '2025-09-17', horaIngreso: '08:40', horaSalida: '17:05', estado: 'Tardanza' },
+  ];
+
+  marcarEntrada() {
+    this.estadoAsistencia = 'Dentro';
+    console.log('Entrada registrada');
+  }
+
+  marcarSalida() {
+    this.estadoAsistencia = 'Fuera';
+    console.log('Salida registrada');
+  }
+
+  descargarReporte() {
+    console.log('Descargando reporte...');
+  }
 }
